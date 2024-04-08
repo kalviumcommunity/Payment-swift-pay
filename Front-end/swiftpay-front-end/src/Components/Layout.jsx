cf92abdbb470c2760d2652470eda2ea7dd9a8ef4
venkatsammm <venkatkishoreraman@gmail.com>
2024-04-08 14:59:32 +0530

    nav-bar

--- Front-end/swiftpay-front-end/src/Components/Layout.jsx
+++ Front-end/swiftpay-front-end/src/Components/Layout.jsx
@@ -1,7 +1,8 @@
 // Layout component
-import React from 'react';
+  import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 
 const Layout = ({ children }) => {
   return (
